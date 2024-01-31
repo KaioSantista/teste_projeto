f3f5ea75e046a1d948b940dd76dc19a3209ad3e5
Kaio Augusto Pereira dos Santos <akaio0202@gmail.com>
2024-01-30 21:51:47 -0300

    prototipo

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let question = array(10)
-question[0] = "kkfcrer"
-question[1] ="jhcedcede"
-
-let pergumta = document.querySelector(' questions')
-let p1 = prompt(" digite 1")
-
-if( p1=1){
-    alert("parabens")
-    pergumta = question[0]
-}
-
+let vfvt = 2
