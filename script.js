b87c2b5b8a2a16faa8d87c1fb52bd4a75fa91165
Kaio Augusto Pereira dos Santos <akaio0202@gmail.com>
2024-02-01 15:17:15 -0300

    java

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-let vfvt = 2
+let pergunta = [
+    {
+
+        n:1,
+        Questao:"2+2=",
+        Alternativas:[
+            
+              {Alternativa:"4",resposta:true},
+              {Alternativa:"6",resposta:false},
+              {Alternativa:"2",resposta:false},
+              {Alternativa:"8",resposta:false}
+            
+
+        ]},
+        {
+         n:2,
+         Questao:"5*3=",
+         Alternativas:
+            [
+              {Alternativa:"10",resposta:false},
+              {Alternativa:"15",resposta:true},
+              {Alternativa:"8",resposta:false},
+              {Alternativa:"9",resposta:false}
+            
+
+            ]
+        
+},{
+    n:3,
+         Questao:"100/10=",
+         Alternativas:
+            [
+              {Alternativa:"50",resposta:false},
+              {Alternativa:"1",resposta:false},
+              {Alternativa:"10",resposta:true},
+              {Alternativa:"80",resposta:false}
+            
+
+            ]
+},{
+    n:4,
+         Questao:"5/5=",
+         Alternativas:
+            [
+              {Alternativa:"10",resposta:false},
+              {Alternativa:"2",resposta:false},
+              {Alternativa:"1",resposta:true},
+              {Alternativa:"4",resposta:false}
+            
+
+            ]
+}]
+
